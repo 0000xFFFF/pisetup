c0d2bb2eb4325cfca10d45bad88b1ccaabedd41e
0000xFFFF <coldmailgg@gmail.com>
2025-02-18 12:48:54 +0100

    git clone

--- setup.sh
+++ setup.sh
@@ -12,3 +12,7 @@ EOF
 xargs -a packages.txt sudo apt-get install -y
 mv -n ~/.bashrc ~/.bashrc.bak
 wget https://raw.githubusercontent.com/0000xFFFF/dotfiles/refs/heads/master/.bashrc -O ~/.bashrc
+
+git clone https://github.com/0000xFFFF/dotfiles
+cp -rf dotfiles/.vip ~/.vip
+cp -rf dotfiles/.vip ~/.config
