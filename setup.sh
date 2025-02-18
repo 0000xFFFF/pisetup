3f38cb1c7d55d0738a6e15c49a8f3e60aa79ef1f
0000xFFFF <coldmailgg@gmail.com>
2025-02-18 12:55:21 +0100

    autoremove

--- setup.sh
+++ setup.sh
@@ -10,6 +10,9 @@ exa
 EOF
 
 xargs -a packages.txt sudo apt-get install -y
+
+sudo apt autoremove -y
+
 mv -n ~/.bashrc ~/.bashrc.bak
 wget https://raw.githubusercontent.com/0000xFFFF/dotfiles/refs/heads/master/.bashrc -O ~/.bashrc
 
