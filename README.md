401f61dae4e023dd0d0db254d3ff93d2f1b4623f
0000xFFFF <coldmailgg@gmail.com>
2025-02-18 12:21:49 +0100

    pkgs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pisetup
 pisetup
+
+```
+bash <(curl -s https://raw.githubusercontent.com/0000xFFFF/pisetup/refs/heads/master/setup.sh)
+```
--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -x
 sudo apt update
 sudo apt upgrade -y
+
+cat << EOF >> packages.txt
+git
+make
+zoxide
+exa
+EOF
+
 xargs -a packages.txt sudo apt-get install
 mv ~/.bashrc ~/.bashrc.bak
 wget https://raw.githubusercontent.com/0000xFFFF/dotfiles/refs/heads/master/.bashrc -O ~/.bashrc
