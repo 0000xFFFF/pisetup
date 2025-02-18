3abc8e598e422c0900948f010141032f745eace3
0000xFFFF <coldmailgg@gmail.com>
2025-02-18 12:14:39 +0100

    bak

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/bash -x
 sudo apt update
 sudo apt upgrade -y
 sudo apt install git make
+mv ~/.bashrc ~/.bashrc.bak
 wget https://raw.githubusercontent.com/0000xFFFF/dotfiles/refs/heads/master/.bashrc -O ~/.bashrc
